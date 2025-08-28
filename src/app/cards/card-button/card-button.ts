c46bf141d99d30fbc952d83d4fff807c317d2e57
GoncalvesAllan (Vaccaro) <allan.goncalves@propex.com.br>
2025-08-28 14:25:19 -0300

    Estilos CSS Dentro do Componente (prop "styles")

--- src/app/cards/card-button/card-button.ts
+++ src/app/cards/card-button/card-button.ts
@@ -4,7 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-card-button',
   standalone: false,
   //templateUrl: './card-button.html',
-  styleUrl: './card-button.scss',
+  //styleUrl: './card-button.scss',
+  styles: [`
+    .card-button{
+      font-size: 15px;
+      text-align: center;
+      margin-top: 10px;
+      border-radius: 25px;
+      background-color: white;
+      cursor: pointer;
+      padding: 10px;
+      font-weight: bold;
+    }
+  `],
   template: `
     <div class="card-button">Adquirir</div> 
   `,
