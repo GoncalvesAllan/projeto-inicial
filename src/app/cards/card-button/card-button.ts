b1f691e9700633b9a872bcf4d52c0de2c88afa06
GoncalvesAllan (Vaccaro) <allan.goncalves@propex.com.br>
2025-08-28 13:32:44 -0300

    Template Dentro do Componente (prop "template")

--- src/app/cards/card-button/card-button.ts
+++ src/app/cards/card-button/card-button.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button',
   standalone: false,
-  templateUrl: './card-button.html',
-  styleUrl: './card-button.scss'
+  //templateUrl: './card-button.html',
+  styleUrl: './card-button.scss',
+  template: `
+    <div class="card-button">Adquirir</div> 
+  `,
 })
 export class CardButton {
 
